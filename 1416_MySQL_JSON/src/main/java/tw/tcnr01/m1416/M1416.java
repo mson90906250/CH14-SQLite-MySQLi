979fa1dbaad8ef69954d27ceabdeaf9c6b8a5083
mson90906250 <l90906250@gmail.com>
2019-01-30 11:20:23 +0800

    1416 只建立layout

---
+++ 1416_MySQL_JSON/src/main/java/tw/tcnr01/m1416/M1416.java
@@ -0,0 +1,13 @@
+package tw.tcnr01.m1416;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class M1416 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.m1416);
+    }
+}
